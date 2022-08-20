f99fe95a10a80e0776bb28b3638d514730934bf1
RobBotzz <111586693+RobBotzz@users.noreply.github.com>
2022-08-20 14:29:06 +0200

    update

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 let password1 = document.getElementById("password1")
 let password2 = document.getElementById("password2")
 
-let plength = document.getElementById("plength")
+let plengthEl = document.getElementById("plength")
 plength.textContent = "TEST"
 
 function generatePasswords() {
+    let plength = getInt(plengthEl.textContent)
+
     password1.textContent = ""
     password2.textContent = ""
-    for(let i = 0; i < 15; i += 1) {
+    for(let i = 0; i < plength; i += 1) {
         password1.textContent += characters[Math.floor(Math.random() * characters.length)]
         password2.textContent += characters[Math.floor(Math.random() * characters.length)]
     }
+}
+
+function getInt(s) {
+    const parsed = parseInt(s, "10")
+    if(isNaN(parsed)) return 0
+    return parsed
 }
 
 
